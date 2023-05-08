43736e87e3b8aabc9d30ab80c89d201fd3f5b727
AbdElRahman Rizq <a.rezk453@gmail.com>
2023-05-09 01:51:16 +0300

    shared button for all files

---
+++ src/shared/actionButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { SelectedPage } from './types' 
+
+type Props = {
+  children: React.ReactNode,
+setSelectedPage:(value:SelectedPage)=>void;
+}
+
+const ActionButton = ({children,setSelectedPage}: Props) => {
+  return (
+    <AnchorLink 
+    className='rounde-md bg-secondary-500 hover:bg-primary-500 hover:text-white px-10 py-2'
+      onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+      </AnchorLink>
+  )
+}
+export default ActionButton
