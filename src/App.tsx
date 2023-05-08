7eca4b80e9100d24d7be6c7b2274032f620e7ff0
AbdElRahman Rizq <a.rezk453@gmail.com>
2023-05-09 02:21:20 +0300

    lunch app

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import Navbar from"@/pages/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+import Home from "@/pages/home";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopPage,setTopPage]=useState<boolean>(false);
+  useEffect(
+    ()=>{
+      const handleScroll=()=>{
+        if(window.scrollY===0)
+        {setTopPage(true);
+          setSelectedPage(SelectedPage.Home)
+        }
+        else 
+        setTopPage(false);
+      }
+      window.addEventListener("scroll",handleScroll);
+      return()=>
+      window.removeEventListener("scroll",handleScroll);
+    },[]
+  )
+
+  return (
+    <div className="app bg-gray-20">
+    <Navbar 
+    isTopPage={isTopPage}
+    selectedPage={selectedPage}
+    setSelectedPage={setSelectedPage}
+    />
+      <Home />
+     </div>
+  )
+}
+
+export default App
