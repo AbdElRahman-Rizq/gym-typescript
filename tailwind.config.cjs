b704caec09c7d25fef298ff52fd96893c7c4c0b1
AbdElRahman Rizq <a.rezk453@gmail.com>
2023-05-19 03:40:27 +0300

    added content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,7 +25,10 @@ module.exports = {
       },
             content: {
         evolvetext: "url('./assets/EvolveText.png')",
-            }
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      }
     },
     screens: {
     xs: "480px",
